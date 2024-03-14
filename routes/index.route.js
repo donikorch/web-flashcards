7a51a2077f8846bbdd8e203f06ba58e5e04f1edb
Evgenij <pigorev2010@yandex.ru>
2024-03-14 17:48:32 +0300

    404

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const path = require('path')
 
 // роуты
 const logReg = require('./views/logreg.route');
@@ -6,7 +7,7 @@ const logReg = require('./views/logreg.route');
 router.use('/', logReg);
 
 router.get('/*', (req, res) => {
-  res.json({ message: '404' });
+  res.redirect('https://atlassianblog.wpengine.com/wp-content/uploads/2017/12/44-incredible-404-error-pages@3x-1560x760.png');
 });
 
 module.exports = router;
